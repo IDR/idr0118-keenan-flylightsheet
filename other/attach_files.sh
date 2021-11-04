41be2613819281f2092412912facbfade517e215
Dominik Lindner <d.lindner@dundee.ac.uk>
2021-11-04 11:11:08 +0000

    Also attach excel files

--- other/attach_files.sh
+++ other/attach_files.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
+projectId=$1
+
 for i in `cat files_to_attach.txt`
 do
 	filename=${i##*/}
-	imagename=${filename%_Seg*}
-	image=`python /uod/idr/metadata/idr-utils/scripts/annotate/find_images.py $imagename Project:2051`
-	python /uod/idr/metadata/idr-utils/scripts/annotate/attach_file.py -m "image/tiff" $i $image
+	imagename=${filename%_*}
+	if [[ $imagename == *Segmented ]]
+	then
+		imagename=${imagename%_Segmented*}
+	fi
+	if [[ $filename == *tif ]]
+	then
+		mtype="image/tiff"
+	else
+		mtype="openmicroscopy.org/idr/analysis/original"
+	fi
+	image=`python /uod/idr/metadata/idr-utils/scripts/annotate/find_images.py $imagename Project:$projectId`
+	python /uod/idr/metadata/idr-utils/scripts/annotate/attach_file.py -m $mtype $i $image
 done
